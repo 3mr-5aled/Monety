7867d28431c792e5834d76dc0fe7d89cca7901b7
3mr-5aled <moroamr2005@gmail.com>
2023-02-23 16:44:37 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,60 @@
-const path = require("path");
+const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-module.exports = {
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production"
+
+  return {
     entry: "./src/app.jsx",
-    // entry: "./src/app.jsx",
     output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js",
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
     },
     module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"],
-                        plugins: ["@babel/plugin-proposal-class-properties"],
-                    },
-                },
+      rules: [
+        {
+          test: /\.jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+              plugins: ["@babel/plugin-proposal-class-properties"],
             },
-            {
-                test: /\.s?css$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-        ],
-    },
-    devtool: "eval-cheap-module-source-map",
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "public"),
+          },
         },
-        historyApiFallback: true,
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "styles.css",
+      }),
+    ],
+    devtool: isProduction ? "source-map" : "inline-cheap-module-source-map",
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+      historyApiFallback: true,
     },
     mode: "development",
-};
+  }
+}
